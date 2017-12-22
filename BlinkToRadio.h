444e3c5d7ea2a72ffd06edad91553adacfe54ca6
zerotrac <734688610@qq.com>
2017-12-22 22:55:31 +0800

    Upload BlinkToRadio.h

--- BlinkToRadio.h
+++ BlinkToRadio.h
@@ -1,27 +1,42 @@
-// $Id: BlinkToRadio.h,v 1.4 2006/12/12 18:22:52 vlahan Exp $
-
 #ifndef BLINKTORADIO_H
 #define BLINKTORADIO_H
 
 enum {
-    AM_BLINKTORADIO = 6,
-    TIMER_PERIOD_MILLI = 250,
+    AM_JOYSTICKMSG   = 6,
+    AM_INITIALIZEMSG = 7,
 
-    JOYSTICK_NOOP = 0,
-    JOYSTICK_UP = 1,
-    JOYSTICK_DOWN = 2,
-    JOYSTICK_LEFT = 3,
+    TIMER_PERIOD_MILLI = 100,
+
+    JOYSTICK_STOP  = 0,
+    JOYSTICK_UP    = 1,
+    JOYSTICK_DOWN  = 2,
+    JOYSTICK_LEFT  = 3,
     JOYSTICK_RIGHT = 4,
 
-    STEER_ANGLE_MIN = 1800,
-    STEER_ANGLE_MAX = 5000
+    STEER_ANGLE_NOOP    = 0,
+    STEER_ANGLE_MIN     = 1800,
+    STEER_ANGLE_DEFAULT = 3000,
+    STEER_ANGLE_MAX     = 5000,
+
+    STEER_ANGLE_DELTA   = 100,
+
+    STEER_TURN_DOWN = 0,
+    STEER_TURN_UP   = 1,
+
+    CAR_ID = 9
 };
 
 typedef nx_struct JoyStickMsg {
     nx_uint16_t JoyStickOp;
+    nx_uint16_t Steer1Status;
+    nx_uint16_t Steer2Status;
+    nx_uint16_t Steer3Status;
+} JoyStickMsg;
+
+typedef nx_struct InitializeMsg {
     nx_uint16_t Steer1Angle;
     nx_uint16_t Steer2Angle;
     nx_uint16_t Steer3Angle;
-}
+} InitializeMsg;
 
 #endif
